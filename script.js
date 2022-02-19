0f22f3356c4a1285a1decbe08ae719ac9c09ad47
Arpit Gupta <55998869+the-coded-bit@users.noreply.github.com>
2022-02-19 13:41:13 +0530

    Tribute to AP

---
+++ script.js
@@ -0,0 +1,25 @@
+const playbtn = document.querySelector('.play');
+const audio = document.querySelector('.audio');
+var isPlaying = false;
+
+playbtn.addEventListener('click', ()=> {
+
+    console.log(`music playing ${isPlaying}`);
+    if(isPlaying){
+        audio.pause();
+        isPlaying = false;
+        playbtn.innerText = 'Play';
+    }
+    else{
+        audio.play();
+        playbtn.innerText = 'Pause';
+        isPlaying = true;
+    }
+});
+
+audio.addEventListener('ended', () =>{
+    isPlaying = false;
+    playbtn.innerText = 'Play';
+})
+
+
